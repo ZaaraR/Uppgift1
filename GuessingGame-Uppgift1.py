802d3d5cafcb1acde872158efa1464222a5ede08
ZaaraR <89464499+ZaaraR@users.noreply.github.com>
2021-09-06 23:03:52 +0200

    Add files via upload

---
+++ GuessingGame-Uppgift1.py
@@ -0,0 +1,42 @@
+# This is a guess the number game.
+
+import random
+def game():
+    guessesTaken = 0
+    print("Hello! What is your name?")
+    myName = input()
+    number = random.randint(1,20)
+    print("Well," + myName + ", I am thinking of a number between 1 and 20")
+
+    while guessesTaken < 6:
+        print("Take a guess.")
+        guess = input()
+        guess = int(guess)
+
+        guessesTaken = guessesTaken + 1
+
+        if guess < number:
+            print("Your guess is too low")
+
+        if guess > number:
+            print("Your guess is too high")
+
+        if guess == number:
+            break
+
+    if guess == number:
+        guessesTaken = str(guessesTaken)
+        print("Good job," + myName + "! You guessed my number in"  + guessesTaken +  "guesses!")
+
+    if guess != number:
+        number = str(number)
+        print("Nope. The number I was thinking of was" + number)
+game()
+
+choose = input("Would you like to play again?\n")
+while choose =="yes":
+    game()
+    if choose == "no":
+        break
+
+
